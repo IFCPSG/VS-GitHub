6528cd9b30b8c6f12474b95e56d3a6cc0cedd206
IFCPSG <149571142+IFCPSG@users.noreply.github.com>
2024-05-29 14:17:15 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+
+        // Obtener los valores de los campos del formulario
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+
+        // Mostrar una alerta con los datos ingresados
+        alert(`Nombre: ${name}\nCorreo Electrónico: ${email}`);
+    });
+});
 
